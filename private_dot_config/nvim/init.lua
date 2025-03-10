babe4208c4cd7946ecafa4de9365e4015b9b23fe
pizza-prosciutto <xhefix@gmail.com>
2025-03-10 21:40:45 +0100

    reduced neovim configuration

---
+++ private_dot_config/nvim/init.lua
@@ -0,0 +1,10 @@
+-- Various options
+--
+local indent = 2
+vim.opt.tabstop = indent        -- Number of spaces that a <Tab> in the file counts for
+vim.opt.shiftwidth = indent     -- Number of spaces to use for auto-indenting
+vim.opt.expandtab = true        -- Use spaces instead of tabs
+vim.opt.softtabstop = indent    -- Number of spaces a <Tab> key inserts
+vim.opt.number = true           -- Display line numbers
+vim.opt.mouse = a               -- Enable mouse
+vim.opt.list = true             -- Display hidden characters
