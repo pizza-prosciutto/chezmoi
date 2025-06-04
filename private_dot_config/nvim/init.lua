4bf03b14c92c174d378f514c131b2c36e5b86823
Viktor Hefler <xhefix@gmail.com>
2025-06-04 10:27:49 +0200

    add a couple of vim options

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -5,10 +5,19 @@ vim.opt.tabstop = indent        -- Number of spaces that a <Tab> in the file cou
 vim.opt.shiftwidth = indent     -- Number of spaces to use for auto-indenting
 vim.opt.expandtab = true        -- Use spaces instead of tabs
 vim.opt.softtabstop = indent    -- Number of spaces a <Tab> key inserts
+vim.opt.smartindent = true      -- Smart indentation (C-like languages)
+vim.opt.autoindent = true       -- Auto indentation (copy indent from previous line)
+
 vim.opt.number = true           -- Display line numbers
 vim.opt.mouse = a               -- Enable mouse
-vim.opt.list = true             -- Display hidden characters
 
+vim.opt.ignorecase = true       -- Ignore case when searching
+vim.opt.incsearch = true        -- Show search results while typing
+vim.opt.smartcase = true        -- Override ignorecase if search contains uppercase
+
+vim.cmd('colorscheme koehler')  -- Set theme
+
+vim.opt.list = true             -- Display hidden characters
 vim.opt.listchars = {
   tab = '→ ',
   trail = '·',
