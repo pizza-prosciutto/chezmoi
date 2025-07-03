5e4927d921c6a1a417c6f6a8e38d63ac7eac11ae
pizza-prosciutto <xhefix@gmail.com>
2025-07-03 17:48:15 +0200

    pacman aliases

--- dot_oh-my-zsh/custom/common.zsh
+++ dot_oh-my-zsh/custom/common.zsh
@@ -26,6 +26,44 @@ setopt APPEND_HISTORY        # Append to history file (Default).
 setopt HIST_NO_STORE         # Don't store history commands.
 setopt HIST_REDUCE_BLANKS    # Remove superfluous blanks from each command line being added to the history.
 
+##################
+# pacman aliases #
+##################
+alias update='sudo pacman -Syu'                 # Full system update
+alias up='sudo pacman -Syu'                     # Shorter alias for update
+alias install='sudo pacman -S'                  # Install a package
+alias remove='sudo pacman -R'                   # Remove a package
+alias purge='sudo pacman -Rns'                  # Remove with dependencies and configs
+alias search='pacman -Ss'                       # Search for a package in repos
+alias list='pacman -Qs'                         # List installed packages matching string
+alias info='pacman -Si'                         # Show info for a package
+alias owns='pacman -Qo'                         # Find which package owns a file
+alias files='pacman -Ql'                        # List files installed by a package
+alias clean='sudo pacman -Rns $(pacman -Qtdq)'  # Remove orphaned packages
+
+function pachelp() {
+    local aliases=(
+        update
+        up
+        install
+        remove
+        purge
+        search
+        list
+        info
+        owns
+        files
+        clean
+    )
+    for alias_name in "${aliases[@]}"; do
+        if alias "$alias_name" &>/dev/null; then
+            alias "$alias_name"
+        else
+            echo "alias $alias_name not found"
+        fi
+    done
+}
+
 #######
 # fzf #
 #######
